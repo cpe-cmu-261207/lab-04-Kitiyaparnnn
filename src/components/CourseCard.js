7d7bbc07963dd54c5dc357047952dd1a38b12ec2
Kitiyaparnnn <parn.takhum@gmail.com>
2020-12-26 21:40:13 +0700

    Finish

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,18 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return (
+    <div class="shadow-md p-3 rounded-3xl m-2 ;box-sizing: content-box">
+      <div>
+        <h2 className="text-xl my-1 text-green-400 italic">course name : {props.vname}</h2>
+        <h1 className="text-xl my-1">course code : {props.vcode}</h1>
+        <h1 className="text-xl my-1">credit : {props.vcredit}</h1>
+        <h2 className="text-xl my-1 text-yellow-300">course grade : {props.vgrade}</h2>
+
+        <div class="text-right ; text-danger">
+          <button onClick={() => { props.del(props.vname) }}>x</button>
+        </div>
+      </div>
+    </div>
+
+  );
 };
